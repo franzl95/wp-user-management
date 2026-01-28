7b7d39a57a9e0092f7ed32930fac8bfa0ba157ae
franzl95 <franzl@jbx.at>
2026-01-28 15:04:45 +0100

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,7 @@
 <?php
-// Simple bootstrap – no WP core test suite
+/**
+ * PHPUnit bootstrap (minimal)
+ */
 
+// Load plugin main file
 require_once dirname( __DIR__ ) . '/wp-user-management.php';
