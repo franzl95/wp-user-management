0c0b8074b017268319ff516f42224925916c5d0a
franzl95 <franzl@jbx.at>
2026-01-28 15:58:35 +0100

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,38 +1,4 @@
 <?php
-/**
- * PHPUnit bootstrap file for WP User Management
- */
+declare(strict_types=1);
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-
-if ( ! $_tests_dir ) {
-    $_tests_dir = '/tmp/wordpress-tests-lib';
-}
-
-/**
- * Load PHPUnit Polyfills (Pflicht seit WP 6.x)
- */
-if ( ! defined( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH' ) ) {
-    define(
-        'WP_TESTS_PHPUNIT_POLYFILLS_PATH',
-        $_tests_dir . '/vendor/yoast/phpunit-polyfills'
-    );
-}
-
-/**
- * Load WordPress test functions
- */
-require_once $_tests_dir . '/includes/functions.php';
-
-/**
- * Manually load the plugin
- */
-function _manually_load_plugin() {
-    require dirname( __DIR__ ) . '/wp-user-management.php';
-}
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-
-/**
- * Start up the WP testing environment
- */
-require $_tests_dir . '/includes/bootstrap.php';
+require_once dirname(__DIR__) . '/wp-user-management.php';
